2f8956121cf0f6a4d6b1a7deb9fbebee293d73a0
neus <02mayolneus@gmail.com>
2026-09-06 20:19:22 +0200

    e

--- src/app/genericComponents/General.ts
+++ src/app/genericComponents/General.ts
@@ -91,7 +91,7 @@ export const VideoContainer = styled.div`
 export const SectionTitle = styled.h2`
   font-size: ${TitleL};
   text-transform: uppercase;
-  text-align: center;
+  text-align: left;
 
   @media (max-width: ${MobileBreakpoint}) {
     font-size: ${MobileTitleM};
