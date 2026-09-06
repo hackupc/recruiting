cde91c255e0514bb940d2c5e7964ad2c30f8152a
neus <02mayolneus@gmail.com>
2026-09-06 20:55:01 +0200

    j

--- src/app/genericComponents/DepartmentInfoCard.tsx
+++ src/app/genericComponents/DepartmentInfoCard.tsx
@@ -83,9 +83,7 @@ const CardForDepartment = styled.div<{ $isActive: boolean }>`
 
   &:hover {
     background: ${(props) =>
-      props.$isActive
-        ? "rgba(0, 0, 0, 0.24)"
-        : "rgba(255, 255, 255, 0.06)"};
+      props.$isActive ? "rgba(0, 0, 0, 0.24)" : "rgba(255, 255, 255, 0.06)"};
   }
 `;
 
