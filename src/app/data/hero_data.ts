6ae8dffb1ced16dd1a42aaa5fe04972d2bf36325
neus <02mayolneus@gmail.com>
2026-09-07 02:28:48 +0200

    opens applis

--- src/app/data/hero_data.ts
+++ src/app/data/hero_data.ts
@@ -3,7 +3,7 @@ import { HeroInformation } from "@data/interfaces";
 export const hero_data: HeroInformation = {
   title: "Uneix-te a l'equip de HackUPC",
   applicationsOpenTextButton: "Apunta’t ara!",
-  applicationsLink: "https://hackersatupc.typeform.com/to/ZBm8aTTg",
+  applicationsLink: "https://hackersatupc.typeform.com/to/yNU733vZ",
   applicationsClosedTextButton: "Les inscripcions estan tancades",
-  applicationsOpen: false,
+  applicationsOpen: true,
 };
