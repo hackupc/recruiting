36e17b5dca9a46a0df6171362ae68b693ee8ce9a
neus mayol <58634634+nmayol@users.noreply.github.com>
2026-09-22 20:58:31 +0200

    Update applications link in hero_data (#10)

--- src/app/data/hero_data.ts
+++ src/app/data/hero_data.ts
@@ -3,7 +3,7 @@ import { HeroInformation } from "@data/interfaces";
 export const hero_data: HeroInformation = {
   title: "UNEIX-TE A L'EQUIP DE HACKUPC",
   applicationsOpenTextButton: "Apunta’t ara! ✨",
-  applicationsLink: "https://hackersatupc.typeform.com/to/yNU733vZ",
+  applicationsLink: "https://hackersatupc.typeform.com/to/SSTb34zW",
   applicationsClosedTextButton: "Les inscripcions estan tancades",
   applicationsOpen: true,
 };
